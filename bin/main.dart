47597b4abcdedf1d871c48c6b9bb796facc2a6e9
DanteYu <aptx113@gmail.com>
2022-03-21 00:31:31 +0800

    get weather tmp

--- bin/main.dart
+++ bin/main.dart
@@ -1,11 +1,11 @@
 import 'weather_api_client.dart';
 
-void main(List<String> args) {
+Future<void> main(List<String> args) async {
   if (args.length != 1) {
     print('Syntax: dart bin/main.dart <city>');
     return;
   }
   final city = args.first;
   final api = WeatherApiClient();
-  api.getLocationId(city);
+  final weather = await api.getWeather(city);
 }
--- bin/weather_api_client.dart
+++ bin/weather_api_client.dart
@@ -2,6 +2,8 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 
+import 'weather.dart';
+
 class WeatherApiClient {
   static const baseUrl = 'https://www.metaweather.com/api';
 
@@ -11,8 +13,23 @@ class WeatherApiClient {
     if (locationResponse.statusCode != 200) {
       throw Exception('Error getting locationId for city: $city');
     }
-    final locationJson = jsonDecode(locationResponse.body);
-    print(locationJson);
-    return 0;
+    final locationJson = jsonDecode(locationResponse.body) as List;
+    return locationJson.first['woeid'];
+  }
+
+  Future<Weather> fetchWeather(int locationId) async {
+    final weatherUrl = Uri.parse('$baseUrl/location/$locationId');
+    final weatherResponse = await http.get(weatherUrl);
+    if (weatherResponse.statusCode != 200) {
+      throw Exception('Error getting weather for location: $locationId');
+    }
+    print(weatherResponse.body);
+    final weatherJson = jsonDecode(weatherResponse.body);
+    return Weather.fromJson(weatherJson);
+  }
+
+  Future<Weather> getWeather(String city) async {
+    final id = await getLocationId(city);
+    return fetchWeather(id);
   }
 }
